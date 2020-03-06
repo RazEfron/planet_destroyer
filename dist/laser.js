0294b2aa0259e8cbc69f5a8f827a547ad3842c28
Raz <razefron01@gmail.com>
2020-03-06 16:21:47 -0500

    laser position fixed

--- dist/laser.js
+++ dist/laser.js
@@ -3,8 +3,8 @@ class Laser {
         this.canvas = canvas;
         this.ctx = ctx;
         this.game = game;
-        this.x = this.game.board.player.position.x + 75;
-        this.y = this.canvas.height - 100;
+        this.x = this.game.board.player.position.x + 53;
+        this.y = this.canvas.height - 80;
         this.speedY = 10;
 
         this.draw = this.draw.bind(this);
